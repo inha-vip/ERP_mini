bd62bfdf280b6f0424d615d5bd093a6345ca968f
sh <61382454+sh-song@users.noreply.github.com>
2020-12-13 17:27:57 +0900

    add nogada

--- KalmanFilter/filterpy_test.py
+++ KalmanFilter/filterpy_test.py
@@ -1,20 +1,28 @@
 import numpy as np
 from filterpy.kalman import KalmanFilter
 from filterpy.common import Q_discrete_white_noise
-
+import time
 
 dt = 0.1
 
 f = KalmanFilter(dim_x = 2, dim_z = 1)
 
 f.x = np.array([[2.], [0.]])
-f.F = np.array([[1.,1.],[0.,1.]])
+f.F = np.array([[1.,dt],[0.,1.]])
 
 f.H = np.array([[1.,0.,]])
-f.P *= 1000.and
+f.P *= 1000.
 f.R = 5
 f.Q = Q_discrete_white_noise(2, dt, .1)
-
+#print(f.Q.size)
+i = 1
+z=2
 while True:
     f.predict()
-    f.update(get_some_meas
+    f.update(z)
+    
+    x = f.x
+    print(x)
+    print(i)
+    i +=1
+    z *=2
